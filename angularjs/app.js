460d24e18cc392cd2a23412b93cefb652bc6e108
Hiep Tran <hiep.tran@bluecomgroup.com>
2016-04-12 16:08:44 +0700

    [demo angular]

---
+++ angularjs/app.js
@@ -0,0 +1,8 @@
+myApp = angular.module('myApp', []);
+    myApp.controller("abc", function($scope){
+   $scope.message ="nhuoi xinh dep";
+
+});
+myApp.controller("def", function($scope){
+    $scope.message ="nhuoi xau xi";
+});
